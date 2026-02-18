94b4dd8a4bac1e247379bc37189e9ad800f5538a
njdb0661 <njdb0661@leeds.ac.uk>
2026-02-18 19:57:58 +0000

    Completed Week 3 Portfolio

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -2,22 +2,31 @@
 #include <stdio.h>
 
 /*
- * Potrfolio submission
- * Name:
- * ID:
+ * Portfolio submission
+ * Name: Jason Chung
+ * ID: 201974903
  */
 
- int main( void ) {
+ int main() {
 
     // define and initialise variables for the problem data 
+   float salary = 36250;
+   int NI_Contribution = 8;
+   int Tax_Contribution = 15;
 
     // calculate the deductions and final take-home salary
+   float NI_Value = salary * (NI_Contribution / 100);
+   float NI_Deducted = salary - NI_Value;
+
+   float taxValue = NI_Deducted * (Tax_Contribution / 100);
+   float finalSalary = NI_Deducted - taxValue;
 
     // Use only these print statement with appropriate formatting and variable names
-    //printf("Salary £...",var_name);
-    //printf("NI contribution £...",var_name);
-    //printf("Tax contribution £...",var_name);
-    //printf("Take home salary £...",var_name);
+
+    printf("Salary £%.2f", salary);
+    printf("NI contribution £%.2f", NI_Value);
+    printf("Tax contribution £%.2f", taxValue);
+    printf("Take home salary £%.2f", finalSalary);
 
     return 0;
  }
